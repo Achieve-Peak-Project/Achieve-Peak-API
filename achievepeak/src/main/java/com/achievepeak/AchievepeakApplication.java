ddf68a8d83fb8736d8e5ee470f6771db22fd6988
jihyun.seo <jihyun.seo.dev@gmail.com>
2025-03-29 13:42:28 +0900

    feat: Add Test API

---
+++ achievepeak/src/main/java/com/achievepeak/AchievepeakApplication.java
@@ -0,0 +1,13 @@
+package com.achievepeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievepeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievepeakApplication.class, args);
+	}
+
+}
---
+++ achievepeak/src/test/java/com/achievepeak/AchievepeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achievepeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievepeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
