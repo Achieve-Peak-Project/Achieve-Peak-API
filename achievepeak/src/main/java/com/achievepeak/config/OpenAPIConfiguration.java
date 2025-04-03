0fe50b576e41464738c7844925c72cf220075445
jihyun.seo <jihyun.seo.dev@gmail.com>
2025-04-03 22:29:28 +0900

    fix: Swagger 페이지를 기본 주소로 접근할 수 있도록 설정

--- achievepeak/src/main/java/com/achievepeak/config/OpenAPIConfiguration.java
+++ achievepeak/src/main/java/com/achievepeak/config/OpenAPIConfiguration.java
@@ -1,26 +1,32 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-package com.achievepeak.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenAPIConfiguration {
-   private static final String API_NAME = "Achieve Peak Application";
-   private static final String API_VERSION = "1.0.0";
-   private static final String API_DESCRIPTION = "Achieve Peak 애플리케이션입니다.";
-
-   public OpenAPIConfiguration() {
-   }
-
-   @Bean
-   public OpenAPI OpenAPIConfig(){
-       return new OpenAPI()
-               .info(new Info()
-                       .title(API_NAME)
-                       .description(API_DESCRIPTION)
-                       .version(API_VERSION));
-   }
-}
+package com.achievepeak.config;  
+  
+import io.swagger.v3.oas.models.OpenAPI;  
+import io.swagger.v3.oas.models.info.Info;  
+import org.springframework.context.annotation.Bean;  
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;  
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;  
+  
+@Configuration  
+public class OpenAPIConfiguration implements WebMvcConfigurer {  
+    private static final String API_NAME = "Achieve Peak Application";  
+    private static final String API_VERSION = "1.0.0";  
+    private static final String API_DESCRIPTION = "Achieve Peak 애플리케이션입니다.";  
+  
+    public OpenAPIConfiguration() {  
+    }  
+  
+    @Bean  
+    public OpenAPI OpenAPIConfig(){  
+        return new OpenAPI()  
+                .info(new Info()  
+                        .title(API_NAME)  
+                        .description(API_DESCRIPTION)  
+                        .version(API_VERSION));  
+    }  
+  
+    @Override  
+    public void addViewControllers(ViewControllerRegistry registry) {  
+        registry.addRedirectViewController("/", "/swagger-ui/index.html#/");  
+    }  
+}  
